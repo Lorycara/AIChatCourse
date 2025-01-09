1546df864bc5b6fcefa23d715abd8a001a74872e
Lorycara <lorycara62@gmail.com>
2025-01-09 14:30:58 +0100

    Initial Commit

---
+++ AIChatCourse/AIChatCourseApp.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseApp.swift
+//  AIChatCourse
+//
+//  Created by Lorenzo Cara on 09/01/25.
+//
+
+import SwiftUI
+
+@main
+struct AIChatCourseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AIChatCourseTests/AIChatCourseTests.swift
@@ -0,0 +1,17 @@
+//
+//  AIChatCourseTests.swift
+//  AIChatCourseTests
+//
+//  Created by Lorenzo Cara on 09/01/25.
+//
+
+import Testing
+@testable import AIChatCourse
+
+struct AIChatCourseTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
